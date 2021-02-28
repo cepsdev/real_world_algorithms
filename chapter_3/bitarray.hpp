73392850577d0a7d28983636f0d9d525006b7dc8
Tomas Prerovsky <tomas.prerovsky@gmail.com>
2021-02-28 23:53:12 +0100

    Very naive but nonetheless working version of a bit array.

--- chapter_3/bitarray.hpp
+++ chapter_3/bitarray.hpp
@@ -6,25 +6,44 @@ struct bit_array{
     static constexpr auto elem_r = elem_w-bits;
     static constexpr elem_t mask = ~ ((~elem_t{}) << bits) ; 
 
-    std::vector<unsigned long long> v;
+    std::vector<elem_t> v;
     std::size_t last_elem{0};
     unsigned short last_bit_pos{0};
 
     elem_t operator [](std::size_t j) const {
         auto byte_pos = (j * bits) / 8;
         auto bit_pos = (j*bits) % 8;
-        
-        elem_t r = *( ((unsigned char*)v.data())+byte_pos);
-        r >>= bit_pos; r &= mask;
-        auto rr = (byte_pos % sizeof(elem_t))*8 + bit_pos + bits;
-        std::cout << "-- " << rr <<" ";
-        if (rr > elem_w){
-            std::cout << "!!!\n";
-
+        auto p = (unsigned char*)v.data();
+        elem_t e = *(elem_t*) ( p + byte_pos);
+        e >>= bit_pos; 
+        e &= mask;        
+        auto r = elem_w - bit_pos;
+        if ( r < bits){
+            r = bits - r;
+            elem_t mask = ~(~elem_t{} << r);
+            elem_t e_n = *(elem_t*)(p+byte_pos + sizeof(elem_t));
+            e_n &= mask;
+            e_n <<= bits - r;
+            return e | e_n;
         }
-        return r;
+        return e;
     }
 
-
-
+    void write_at(std::size_t j, elem_t value){
+        auto t = value;
+        auto byte_pos = (j * bits) / 8;
+        auto bit_pos = (j*bits) % 8;
+        auto m = mask;
+        m <<= bit_pos; m = ~m;
+        auto p = (unsigned char*)v.data();
+        *(elem_t*)(p + byte_pos) &= m;
+        t <<= bit_pos;
+        *(elem_t*)(p + byte_pos) |= t;
+        if(bit_pos + bits > elem_w){
+            auto remain =  bits - (elem_w - bit_pos); 
+            elem_t u = value >> (bits-remain);
+            *(elem_t*)(p + byte_pos + sizeof(elem_t)) &= (~elem_t{1} << remain);
+            *(elem_t*)(p + byte_pos + sizeof(elem_t)) |= ( ~(~elem_t{1} << remain) & u); 
+        }
+    }
 };
